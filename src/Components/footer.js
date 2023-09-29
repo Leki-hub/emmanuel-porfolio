c361272b37de7d541f71e9dff3cfaef51e06ce81
Leki <lekisintole82@gmail.com>
2023-09-29 11:08:15 +0300

    all pages

---
+++ src/Components/footer.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <>
+     	<footer id="footer-copyright" className="footer-copyright">
+			<div className="container">
+				<div className="hm-footer-copyright text-center">
+					<p>
+						&copy; copyright yourname. design and developed by <a href="https://www.themesine.com/">themesine</a>
+					</p>
+				</div>
+			</div>
+
+			<div id="scroll-Top">
+				<div className="return-to-top">
+					<i className="fa fa-angle-up " id="scroll-top" ></i>
+				</div>
+				
+			</div>
+        </footer>
+    
+    </>
+  )
+}
