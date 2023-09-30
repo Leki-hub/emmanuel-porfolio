27a3e5f2c5de40ed463ec41c15b3bda309f0f07e
Leki <lekisintole82@gmail.com>
2023-09-30 22:27:43 +0300

    github link

---
+++ footer.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <>
+     	<footer id="footer-copyright" className="footer-copyright">
+			<div className="container">
+				<div className="hm-footer-copyright text-center">
+					<p>
+						&copy; copyright Leki. design and developed by <a href="https://github.com/Leki-hub">Emmanuel</a>
+					</p>
+				</div>
+			</div>
+		
+        </footer>
+    
+    </>
+  )
+}
